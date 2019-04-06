a975fa5aecc2fd94a94b25b05b2566b93bc793e7
Joshua Eckroth <jeckroth@stetosn.edu>
2019-04-06 05:09:59 +0000

    initial commit

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject dig "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [compojure "1.6.1"]
+                 [hiccup "1.0.5"]]
+  :profiles {:dev
+             {:dependencies
+              [[javax.servlet/servlet-api "2.5"]]}}
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler dig.core/handler}
+  :repl-options {:init-ns dig.core})
+
